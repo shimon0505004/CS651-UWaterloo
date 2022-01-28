e5f5ade2623acd3424821fb4594756b789db5caf
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-01-28 13:17:54 -0500

    Updated PairsPMI.java to inclear threshold, considering only first 40 lines of each word and generating co-occuring pairs.

--- src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs451/a1/PairsPMI.java
@@ -20,7 +20,7 @@ import io.bespin.java.util.Tokenizer;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -29,8 +29,6 @@ import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
@@ -39,79 +37,112 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.ParserProperties;
 import tl.lin.data.pair.PairOfStrings;
+import tl.lin.data.map.HMapKIW;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
-public class PairsPMI  extends Configured implements Tool {
+/**
+ * <p>
+ * Implementation of the "pairs" algorithm for computing co-occurrence matrices from a large text
+ * collection. This algorithm is described in Chapter 3 of "Data-Intensive Text Processing with 
+ * MapReduce" by Lin &amp; Dyer, as well as the following paper:
+ * </p>
+ *
+ * <blockquote>Jimmy Lin. <b>Scalable Language Processing Algorithms for the Masses: A Case Study in
+ * Computing Word Co-occurrence Matrices with MapReduce.</b> <i>Proceedings of the 2008 Conference
+ * on Empirical Methods in Natural Language Processing (EMNLP 2008)</i>, pages 419-428.</blockquote>
+ *
+ * @author Jimmy Lin
+ */
+public class PairsPMI extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(PairsPMI.class);
 
-  private static final class MyMapper extends Mapper<LongWritable, Text, PairOfStrings, FloatWritable> {
-    private static final FloatWritable ONE = new FloatWritable(1);
-    private static final PairOfStrings BIGRAM = new PairOfStrings();
+  private static final class FirstMapper extends Mapper<LongWritable, Text, PairOfStrings, IntWritable> {
+    private static final PairOfStrings PAIR = new PairOfStrings();
+    private static final IntWritable ONE = new IntWritable(1);
+    private static final HMapKIW<PairOfStrings> DUPLICATECHECKERSET = new HMapKIW<PairOfStrings>();
+
+    //private int window = 2;
+
+    @Override
+    public void setup(Context context) {
+      //window = context.getConfiguration().getInt("window", 2);
+    }
 
     @Override
     public void map(LongWritable key, Text value, Context context)
         throws IOException, InterruptedException {
       List<String> tokens = Tokenizer.tokenize(value.toString());
+      DUPLICATECHECKERSET.clear();
 
-      if (tokens.size() < 2) return;
-      for (int i = 1; i < tokens.size(); i++) {
-        BIGRAM.set(tokens.get(i - 1), tokens.get(i));
-        context.write(BIGRAM, ONE);
+      for (int i = 0; i < Math.min(tokens.size(), 40); i++) {
+        for(int j = 0; j < Math.min(tokens.size(), 40); j++)  {           // Ensure only the first 40 words in each line
+          if (i == j) continue;
+          if (tokens.get(i).compareTo(tokens.get(j) == 0) continue;       // When Line is like A B C A B C, avoid pairs like A A
 
-        BIGRAM.set(tokens.get(i - 1), "*");
-        context.write(BIGRAM, ONE);
+          PAIR.set(tokens.get(i), tokens.get(j));
+          if(!DUPLICATECHECKERSET.containsKey(PAIRS)){
+            context.write(PAIR, ONE);                                    // When Line is like A B C A B C, Take co-occuring pair (A,B) only once  
+            DUPLICATECHECKERSET.put(PAIR, ONE);
+          }
+        }
       }
+
+      DUPLICATECHECKERSET.clear();
+
     }
   }
 
-  private static final class MyCombiner extends
-      Reducer<PairOfStrings, FloatWritable, PairOfStrings, FloatWritable> {
-    private static final FloatWritable SUM = new FloatWritable();
+  private static final class FirstCombiner extends
+      Reducer<PairOfStrings, IntWritable, PairOfStrings, IntWritable> {
+    private static final IntWritable SUM = new IntWritable();
 
     @Override
-    public void reduce(PairOfStrings key, Iterable<FloatWritable> values, Context context)
+    public void reduce(PairOfStrings key, Iterable<IntWritable> values, Context context)
         throws IOException, InterruptedException {
+      Iterator<IntWritable> iter = values.iterator();
       int sum = 0;
-      Iterator<FloatWritable> iter = values.iterator();
       while (iter.hasNext()) {
         sum += iter.next().get();
       }
+
       SUM.set(sum);
       context.write(key, SUM);
     }
   }
 
-  private static final class MyReducer extends
-      Reducer<PairOfStrings, FloatWritable, PairOfStrings, FloatWritable> {
-    private static final FloatWritable VALUE = new FloatWritable();
-    private float marginal = 0.0f;
+  private static final class FirstReducer extends
+      Reducer<PairOfStrings, IntWritable, PairOfStrings, IntWritable> {
+    private static final IntWritable SUM = new IntWritable();
+    private int threshold = 1;
 
     @Override
-    public void reduce(PairOfStrings key, Iterable<FloatWritable> values, Context context)
+    public void setup(Context context) {
+      threshold = context.getConfiguration().getInt("threshold", 1);
+    }
+
+    @Override
+    public void reduce(PairOfStrings key, Iterable<IntWritable> values, Context context)
         throws IOException, InterruptedException {
-      float sum = 0.0f;
-      Iterator<FloatWritable> iter = values.iterator();
+      Iterator<IntWritable> iter = values.iterator();
+      int sum = 0;
       while (iter.hasNext()) {
         sum += iter.next().get();
       }
 
-      if (key.getRightElement().equals("*")) {
-        VALUE.set(sum);
-        context.write(key, VALUE);
-        marginal = sum;
-      } else {
-        VALUE.set(sum / marginal);
-        context.write(key, VALUE);
-      }
+      SUM.set(sum);
+
+      if(SUM > threshold)                       //To reduce the number of spurious pairs
+        context.write(key, SUM);  
+
     }
   }
 
-  private static final class MyPartitioner extends Partitioner<PairOfStrings, FloatWritable> {
+  private static final class FirstPartitioner extends Partitioner<PairOfStrings, IntWritable> {
     @Override
-    public int getPartition(PairOfStrings key, FloatWritable value, int numReduceTasks) {
+    public int getPartition(PairOfStrings key, IntWritable value, int numReduceTasks) {
       return (key.getLeftElement().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
   }
@@ -131,8 +162,8 @@ public class PairsPMI  extends Configured implements Tool {
     @Option(name = "-reducers", metaVar = "[num]", usage = "number of reducers")
     int numReducers = 1;
 
-    @Option(name = "-textOutput", usage = "use TextOutputFormat (otherwise, SequenceFileOutputFormat)")
-    boolean textOutput = false;
+    @Option(name = "-threshold", metaVar = "[num]", usage = "threshold of co-occurrence")
+    int threshold = 1;
   }
 
   /**
@@ -151,15 +182,21 @@ public class PairsPMI  extends Configured implements Tool {
       return -1;
     }
 
-    LOG.info("Tool name: " + PairsPMI.class.getSimpleName());
+    LOG.info("Tool: " + PairsPMI.class.getSimpleName());
     LOG.info(" - input path: " + args.input);
     LOG.info(" - output path: " + args.output);
-    LOG.info(" - num reducers: " + args.numReducers);
-    LOG.info(" - text output: " + args.textOutput);
+    LOG.info(" - threshold : " + args.threshold);
+    LOG.info(" - number of reducers: " + args.numReducers);
 
     Job job = Job.getInstance(getConf());
     job.setJobName(PairsPMI.class.getSimpleName());
     job.setJarByClass(PairsPMI.class);
+
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(args.output);
+    FileSystem.get(getConf()).delete(outputDir, true);
+
+    job.getConfiguration().setInt("threshold", args.threshold);
 
     job.setNumReduceTasks(args.numReducers);
 
@@ -167,23 +204,14 @@ public class PairsPMI  extends Configured implements Tool {
     FileOutputFormat.setOutputPath(job, new Path(args.output));
 
     job.setMapOutputKeyClass(PairOfStrings.class);
-    job.setMapOutputValueClass(FloatWritable.class);
+    job.setMapOutputValueClass(IntWritable.class);
     job.setOutputKeyClass(PairOfStrings.class);
-    job.setOutputValueClass(FloatWritable.class);
-    if (args.textOutput) {
-      job.setOutputFormatClass(TextOutputFormat.class);
-    } else {
-      job.setOutputFormatClass(SequenceFileOutputFormat.class);
-    }
+    job.setOutputValueClass(IntWritable.class);
 
-    job.setMapperClass(MyMapper.class);
-    job.setCombinerClass(MyCombiner.class);
-    job.setReducerClass(MyReducer.class);
-    job.setPartitionerClass(MyPartitioner.class);
-
-    // Delete the output directory if it exists already.
-    Path outputDir = new Path(args.output);
-    FileSystem.get(getConf()).delete(outputDir, true);
+    job.setMapperClass(FirstMapper.class);
+    job.setCombinerClass(FirstCombiner.class);
+    job.setReducerClass(FirstReducer.class);
+    job.setPartitionerClass(FirstPartitioner.class);
 
     long startTime = System.currentTimeMillis();
     job.waitForCompletion(true);
