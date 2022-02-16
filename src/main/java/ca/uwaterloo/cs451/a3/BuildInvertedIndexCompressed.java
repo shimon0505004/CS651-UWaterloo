8b0187bdef1a58433ba93c6967d9884f14429960
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-02-16 17:13:56 -0500

    Fixed dependencies in BuildInvertedIndexCompressed.java for A3, added Partitioner dependencies.

--- src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
+++ src/main/java/ca/uwaterloo/cs451/a3/BuildInvertedIndexCompressed.java
@@ -27,6 +27,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableUtils;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
