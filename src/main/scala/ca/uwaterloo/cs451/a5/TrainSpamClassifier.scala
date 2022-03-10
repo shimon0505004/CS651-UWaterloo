d3893f090535596341b0c7174e05363712401d47
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-03-10 03:25:52 -0500

    Processing command line input in TrainSpamClassifier for taking input file and outputing models.

--- src/main/scala/ca/uwaterloo/cs451/a5/TrainSpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/TrainSpamClassifier.scala
@@ -12,3 +12,62 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 import math._
 
+
+class TrainSpamClassifierConf(args: Seq[String]) extends ScallopConf(args) {
+    mainOptions = Seq(input, model)
+    val input = opt[String]("input", descr = "input path", required = true)
+    val model = opt[String]("model", descr = "model", required = true)
+    verify()
+}
+
+object TrainSpamClassifier extends Tokenizer {
+
+    val log = Logger.getLogger(getClass().getName())
+
+    // w is the weight vector (make sure the variable is within scope)
+    val w = Map[Int, Double]()
+
+    // Scores a document based on its list of features.
+    def spamminess(features: Array[Int]) : Double = {
+        var score = 0d
+        features.foreach(f => if (w.contains(f)) score += w(f))
+        score
+    }
+
+    def main(argv: Array[String]) {
+        val args = new TrainSpamClassifierConf(argv)
+
+        log.info("Input: " + args.input())
+        log.info("Model Path: " + args.model())
+
+        val textFile = sc.textFile(args.input())
+
+        val trained = textFile.map(line =>{
+            // Parse input
+            // ..
+            (0, (docid, isSpam, features))
+            }).groupByKey(1)
+
+        /*
+        // This is the main learner:
+        val delta = 0.002
+
+        // For each instance...
+        val isSpam = ...   // label
+        val features = ... // feature vector of the training instance
+
+        // Update the weights as follows:
+        val score = spamminess(features)
+        val prob = 1.0 / (1 + exp(-score))
+        features.foreach(f => {
+            if (w.contains(f)) {
+                w(f) += (isSpam - prob) * delta
+            } else {
+                w(f) = (isSpam - prob) * delta
+            }
+        })
+        */
+
+        trained.saveAsTextFile(args.model())
+    }
+}
