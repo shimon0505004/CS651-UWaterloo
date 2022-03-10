8d4b41b7cbfad167e397a98f30cb41d37a6cf185
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-03-10 00:31:09 -0500

    Initializing scala files for A5

---
+++ src/main/scala/ca/uwaterloo/cs451/a5/ApplySpamClassifier.scala
@@ -0,0 +1,14 @@
+
+
+
+package ca.uwaterloo.cs451.a5
+
+import io.bespin.scala.util.Tokenizer
+
+import org.apache.log4j._
+import org.apache.hadoop.fs._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.rogach.scallop._
+import math._
+
