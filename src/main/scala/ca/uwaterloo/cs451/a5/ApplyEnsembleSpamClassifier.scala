d70473351ea4f94292731f8ee94058374edcc972
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-03-11 00:41:55 -0500

    Created conf method for handling command line options for ensemble classifier for A5

--- src/main/scala/ca/uwaterloo/cs451/a5/ApplyEnsembleSpamClassifier.scala
+++ src/main/scala/ca/uwaterloo/cs451/a5/ApplyEnsembleSpamClassifier.scala
@@ -12,3 +12,11 @@ import org.apache.spark.SparkConf
 import org.rogach.scallop._
 import math._
 
+class ApplyEnsembleSpamClassifierConf(args: Seq[String]) extends ScallopConf(args) {
+    mainOptions = Seq(input, output, model, method)
+    val input = opt[String]("input", descr = "input path", required = true)
+    val output = opt[String]("output", descr = "output path", required = true)
+    val model = opt[String]("model", descr = "model", required = true)
+    val method = opt[String]("method", descr = "method : average/vote", required = true)
+    verify()
+}
