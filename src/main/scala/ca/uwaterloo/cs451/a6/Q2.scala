5a9f097c087111dccf52756b42f6af716440c9d3
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-03-22 04:51:05 -0400

    Fixed problem with Q2 A6, solved.

--- src/main/scala/ca/uwaterloo/cs451/a6/Q2.scala
+++ src/main/scala/ca/uwaterloo/cs451/a6/Q2.scala
@@ -39,48 +39,56 @@ object Q2{
 
         val isParquet:Boolean = args.parquet()
         val limit = 20
+        val o_clerkPos = 6
+        val o_orderkeyPos = 0
+        val l_orderkeyPos = 0
+        val l_shipdatePos = 10
 
         val queryResult  = if(!isParquet){
             //Process as TXT file
             val lineitemRDD = sparkSession.sparkContext.textFile(args.input()+"/lineitem.tbl")
             val ordersRDD = sparkSession.sparkContext.textFile(args.input()+"/orders.tbl")
             
-            val lineItemProjection = lineitemRDD.map(row => row.split('|'))
-                .filter(_.apply(10).equals(date))
-                .map(row => (row.apply(0), row.apply(10)))
+            val lineItemProjection = lineitemRDD.map(line => line.split('|'))
+                .filter(_.apply(l_shipdatePos).equals(date))
+                .map(line => (line.apply(l_orderkeyPos).toInt, line.apply(l_shipdatePos)))
 
-            val ordersProjection = ordersRDD.map(row => {
-                val arr = row.split('|')
-                (arr.apply(0), arr.apply(5))
+            val ordersProjection = ordersRDD.map(line => {
+                val row = line.split('|')
+                (row.apply(o_orderkeyPos).toInt, row.apply(o_clerkPos))
             })
 
             lineItemProjection.cogroup(ordersProjection)
                 .filter(_._2._1.size > 0)
                 .filter(_._2._2.size > 0)
-                .map{case (o_clerk,value) => {
-                    val o_orderkey = value._2.toList
-                    (o_orderkey, o_clerk)
+                .map{case (o_orderkey,value) => {
+                    val o_clerk = value._2.toList.apply(0)
+                    (o_clerk, (value._1.toList.apply(0), o_orderkey))
                 }}
-                .sortBy(_._2)
+                .sortBy(_._2._2)
                 .take(limit)
             
         }else{
             val lineitemRDD = sparkSession.read.parquet(args.input()+"/lineitem").rdd
             val ordersRDD = sparkSession.read.parquet(args.input()+"/orders").rdd
-            val lineItemProjection:RDD[(Int, String)] = lineitemRDD.filter(_.getString(10).equals(date)).map(row => (row.apply(0), row.apply(10)))
-            val ordersProjection:RDD[(Int, String)] = ordersRDD.map(row => (row.apply(0), row.apply(5)))
+            
+            val lineItemProjection:RDD[(Int, String)] = lineitemRDD.filter(_.getString(l_shipdatePos).equals(date))
+                                                                    .map(row => (row.getInt(l_orderkeyPos), row.getString(l_shipdatePos)))
+
+            val ordersProjection:RDD[(Int, String)] = ordersRDD.map(row => (row.getInt(o_orderkeyPos), row.getString(o_clerkPos)))
 
             lineItemProjection.cogroup(ordersProjection)
                 .filter(_._2._1.size > 0)
                 .filter(_._2._2.size > 0)
-                .map{case (o_clerk,value) => {
-                    val o_orderkey = value._2.toList
-                    (o_orderkey, o_clerk)
+                .map{case (o_orderkey,value) => {
+                    val o_clerk = value._2.toList.apply(0)
+                    (o_clerk, (value._1.toList.apply(0), o_orderkey))
                 }}
-                .sortBy(_._2)
+                .sortBy(_._2._2)
                 .take(limit)
         }
 
-        println(s"ANSWER=${queryResult}")
+        queryResult.foreach(row => println("("+row._1+","+row._2+")"))
+        
     }
 } 
