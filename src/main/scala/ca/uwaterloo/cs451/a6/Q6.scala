a8679684153e97af2c5a401bb72f760e5311aec0
ssarefin <ssarefinshimon@uwaterloo.ca>
2022-03-25 00:20:51 -0400

    Committing initial files for Q6 A6

--- src/main/scala/ca/uwaterloo/cs451/a6/Q6.scala
+++ src/main/scala/ca/uwaterloo/cs451/a6/Q6.scala
@@ -14,7 +14,7 @@ import org.rogach.scallop._
 import java.io._  
 import math._
 
-class Q6Conf(args: Seq[String]) extends ScallopConf(args) {
+class Q4Conf(args: Seq[String]) extends ScallopConf(args) {
     mainOptions = Seq(input, date, text, parquet)
     val input = opt[String]("input", descr = "Input path", required = true)
     val date = opt[String]("date", descr = "Date in YYYY-MM-DD format", required = true)
@@ -23,11 +23,11 @@ class Q6Conf(args: Seq[String]) extends ScallopConf(args) {
     verify()
 }
 
-object Q6{
+object Q4{
     val log = Logger.getLogger(getClass().getName())
 
     def main(argv: Array[String]){
-        val args = new Q6Conf(argv)
+        val args = new Q4Conf(argv)
 
         log.info("Input: " + args.input())
         log.info("Date: " + args.date())
@@ -38,57 +38,93 @@ object Q6{
         val date:String = args.date()
 
         val isParquet:Boolean = args.parquet()
-        val limit = 20
-        val o_clerkPos = 6
-        val o_orderkeyPos = 0
-        val l_orderkeyPos = 0
-        val l_shipdatePos = 10
 
-        val queryResult  = if(!isParquet){
-            //Process as TXT file
-            val lineitemRDD = sparkSession.sparkContext.textFile(args.input()+"/lineitem.tbl")
-            val ordersRDD = sparkSession.sparkContext.textFile(args.input()+"/orders.tbl")
-            
-            val lineItemProjection = lineitemRDD.map(line => line.split('|'))
-                .filter(_.apply(l_shipdatePos).equals(date))
-                .map(line => (line.apply(l_orderkeyPos).toInt, line.apply(l_shipdatePos)))
+        
+        val l_orderkeyPos =         0
+        val l_partkeyPos =          1
+        val l_suppkeyPos =          2
+        val l_linenumberPos =       3
+        val l_quantityPos =         4
+        val l_extendedpricePos =    5
+        val l_discountPos =         6
+        val l_taxPos =              7
+        val l_returnflagPos =       8        
+        val l_linestatusPos =       9
+        val l_shipdatePos =         10
+        val l_commitdatePos =       11
+        val l_receiptdatePos =      12
+        val l_shipinstructPos =     13
+        val l_shipmodePos =         14
+        val l_commentPos =          15        
 
-            val ordersProjection = ordersRDD.map(line => {
-                val row = line.split('|')
-                (row.apply(o_orderkeyPos).toInt, row.apply(o_clerkPos))
-            })
 
-            lineItemProjection.cogroup(ordersProjection)
-                .filter(_._2._1.size > 0)
-                .filter(_._2._2.size > 0)
-                .map{case (o_orderkey,value) => {
-                    val o_clerk = value._2.toList.apply(0)
-                    (o_clerk, (value._1.toList.apply(0), o_orderkey))
-                }}
-                .sortBy(_._2._2)
-                .take(limit)
-            
+        val lineItemProjection = if(!isParquet){
+            sparkSession.sparkContext.textFile(args.input()+"/lineitem.tbl")
+                        .map(line => line.split('|'))
+                        .filter(_.apply(l_shipdatePos).equals(date))
+                        .map(line => (line.apply(l_orderkeyPos).toInt, line.apply(l_quantityPos).toLong))
+                        .map(row => {
+                            val l_returnflag = row.apply(l_returnflagPos).toInt
+                            val l_linestatus = row.apply(l_linestatusPos)
+                            val l_quantity = row.apply(l_quantityPos).toDouble
+                            val l_extendedprice = row.apply(l_extendedpricePos).toDouble
+                            val disc_price = l_extendedprice * (1 - row.apply(l_discountPos).toDouble)
+                            val sum_charge = l_extendedprice * (1 - row.apply(l_discountPos).toDouble) * (1 + row.apply(l_taxPos).toDouble)
+                            val l_discount = row.apply(l_discountPos).toDouble
+                            ((l_returnflag, l_linestatus), (l_quantity, l_extendedprice, disc_price, sum_charge, l_discount, 1))
+                        })
         }else{
-            val lineitemRDD = sparkSession.read.parquet(args.input()+"/lineitem").rdd
-            val ordersRDD = sparkSession.read.parquet(args.input()+"/orders").rdd
-            
-            val lineItemProjection:RDD[(Int, String)] = lineitemRDD.filter(_.getString(l_shipdatePos).equals(date))
-                                                                    .map(row => (row.getInt(l_orderkeyPos), row.getString(l_shipdatePos)))
+            sparkSession.read.parquet(args.input()+"/lineitem").rdd
+                        .filter(_.getString(l_shipdatePos).equals(date))
+                        .map(row => {
+                            val l_returnflag = row.getInt(l_returnflagPos)
+                            val l_linestatus = row.getString(l_linestatusPos)
+                            val l_quantity = row.getDouble(l_quantityPos)
+                            val l_extendedprice = row.getDouble(l_extendedpricePos)
+                            val disc_price = l_extendedprice * (1 - row.getDouble(l_discountPos))
+                            val sum_charge = l_extendedprice * (1 - row.getDouble(l_discountPos)) * (1 + row.getDouble(l_taxPos))
+                            val l_discount = row.getDouble(l_discountPos)
+                            ((l_returnflag, l_linestatus), (l_quantity, l_extendedprice, disc_price, sum_charge, l_discount, 1))
+                        })
+         }
 
-            val ordersProjection:RDD[(Int, String)] = ordersRDD.map(row => (row.getInt(o_orderkeyPos), row.getString(o_clerkPos)))
+        val queryResult = lineItemProjection.reduceByKey(((l_quantity1, l_extendedprice1, disc_price1, sum_charge1, l_discount1, count1),
+                                                            (l_quantity2, l_extendedprice2, disc_price2, sum_charge2, l_discount2, count2)) => ((l_quantity1+l_quantity2), 
+                                                                                                                                                (l_extendedprice1+l_extendedprice2), 
+                                                                                                                                                (disc_price1+disc_price2), 
+                                                                                                                                                (sum_charge1+sum_charge2), 
+                                                                                                                                                (l_discount1+l_discount2), 
+                                                                                                                                                (count1+count2)))
+                                            .map(((l_returnflag, l_linestatus), (sum_qty, sum_base_price, sum_disc_price, sum_charge, sum_discount, count)) => ((l_returnflag, l_linestatus), (sum_qty, sum_base_price, sum_disc_price, sum_charge, sum_qty/count, sum_base_price/count , sum_discount/count, count)))
+                                            .collect
 
-            lineItemProjection.cogroup(ordersProjection)
-                .filter(_._2._1.size > 0)
-                .filter(_._2._2.size > 0)
-                .map{case (o_orderkey,value) => {
-                    val o_clerk = value._2.toList.apply(0)
-                    (o_clerk, (value._1.toList.apply(0), o_orderkey))
-                }}
-                .sortBy(_._2._2)
-                .take(limit)
+        queryResult.foreach{case ((l_returnflag, l_linestatus), (sum_qty, sum_base_price, sum_disc_price, sum_charge, avg_qty, avg_base_price , avg_discount, count_order)) => {
+            println("("l_returnflag +","+ l_linestatus +","+ sum_qty +","+ sum_base_price +","+ sum_disc_price +","+ sum_charge +","+ avg_qty +","+ avg_base_price +","+ avg_discount +","+ count_order +")")
+        }}
+        
+        
+        if(isParquet){
+            val lineitemDF = sparkSession.read.parquet(args.input()+"/lineitem")
+
+            updatedlineitemDF.createOrReplaceTempView("lineitem")
+            val result = sparkSession.sql("""select
+                                                l_returnflag,
+                                                l_linestatus,
+                                                sum(l_quantity) as sum_qty,
+                                                sum(l_extendedprice) as sum_base_price,
+                                                sum(l_extendedprice*(1-l_discount)) as sum_disc_price,
+                                                sum(l_extendedprice*(1-l_discount)*(1+l_tax)) as sum_charge,
+                                                avg(l_quantity) as avg_qty,
+                                                avg(l_extendedprice) as avg_price,
+                                                avg(l_discount) as avg_disc,
+                                                count(*) as count_order
+                                            from lineitem
+                                            where
+                                                l_shipdate = '"""+date+""""'
+                                            group by l_returnflag, l_linestatus""")
+                                     .show(200, false)
+
         }
-
-        queryResult.foreach(row => println("("+row._1+","+row._2+")"))
         
     }
 } 
